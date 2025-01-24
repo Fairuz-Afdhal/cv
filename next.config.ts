a55bc842e920476f8c3531c9e725739f25b49fb6
Fairuz <fairuzzafdhal@gmail.com>
2025-01-24 18:42:42 +0700

    Use static page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Enable static export
+  images: {
+    unoptimized: true, // Disable Image Optimization for static export
+  }
 };
 
 export default nextConfig;
